178d0313306d98469fb402449f008d61316558ab
Eliel Oliveira <elielsjc@hotmail.com>
2022-08-19 16:14:32 -0300

    Requisitos 8 e 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const handleSearch = () => {
+    // função para esconder e mostrar o componente de busca
+  };
+
   return (
     <header className="header">
       <h1 data-testid="page-title">{ headerTitle }</h1>
-      <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
-      <img src={ searchIcon } alt="searchIcon" data-testid="search-top-btn" />
+      <Link to="/profile">
+        <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
+      </Link>
+      <button type="button" onClick={ handleSearch }>
+        <img
+          src={ searchIcon }
+          alt="searchIcon"
+          data-testid="search-top-btn"
+        />
+      </button>
+      <SearchBar />
     </header>
   );
 }
