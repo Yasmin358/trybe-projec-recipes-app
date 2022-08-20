e868150001dd6e8e4a3ac8c1ffdb1cfb238f634c
Anderson Correa <andersonfpcorrea@gmail.com>
2022-08-20 10:46:04 -0300

    requisito 16 OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import GlobalProvider from './context/GlobalProvider';
-// import './App.css';
+import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
@@ -11,6 +11,7 @@ import Recipes from './pages/Recipes';
 import DrinksRecipes from './pages/DrinksRecipes';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/drinks/:id" component={ DrinksRecipes } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route path="/" component={ Footer } />
       </GlobalProvider>
     </Switch>
   );
