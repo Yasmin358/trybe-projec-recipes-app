1931ba798d5cc836dc32d99a0fa7ca35241a98d9
RicardoCaselati <ricardocaselati@gmail.com>
2022-08-22 14:56:38 -0300

    Req10-ok

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,43 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <input data-testid="search-input" />
+    <form>
+      <input
+        type="text"
+        placeholder="Search Recipe"
+        data-testid="search-input"
+      />
+      <label htmlFor="Ingredientes">
+        Ingredientes
+        <input
+          id="Ingredientes"
+          type="radio"
+          data-testid="ingredient-search-radio"
+        />
+      </label>
+      <label htmlFor="name">
+        Name
+        <input
+          id="name"
+          type="radio"
+          data-testid="name-search-radio"
+        />
+      </label>
+      <label htmlFor="firstLetter">
+        First Letter
+        <input
+          id="firstLetter"
+          type="radio"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      <button
+        type="submit"
+        data-testid="exec-search-btn"
+      >
+        Pesquisar
+      </button>
+    </form>
   );
 }
 
