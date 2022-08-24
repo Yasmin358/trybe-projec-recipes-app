6a600746261f4b75b9192da371682dd60ef9cf2b
Anderson Correa <andersonfpcorrea@gmail.com>
2022-08-24 13:14:27 -0300

    req 29 (faltam branches e functions)

--- src/test/RecipesDetails.test.js
+++ src/test/RecipesDetails.test.js
@@ -1,14 +1,26 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../App';
+import RecipesDetails from '../pages/RecipesDetails';
+import GlobalContext from '../context/GlobalContext';
+
+const store = {
+
+};
 
 describe('Tests for the RecipesDetails component', () => {
   it('checks if the component renders accordingly', () => {
     render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>, document.createElement('div'),
+      <GlobalContext.Provider value={ store }>
+        <BrowserRouter>
+          <RecipesDetails
+            {
+              ...{ match: { params: { id: '52772' } },
+                history: { location: { pathname: '/foods/52772' } } }
+            }
+          />
+        </BrowserRouter>
+      </GlobalContext.Provider>, document.createElement('div'),
     );
   });
 });
