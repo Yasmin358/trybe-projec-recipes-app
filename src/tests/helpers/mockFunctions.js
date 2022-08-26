a1d828322e3bc25946ba419160e43b1234cb27d5
Anderson Correa <andersonfpcorrea@gmail.com>
2022-08-26 20:00:43 -0300

    atualiza o arquivo de funções mocks

--- src/tests/helpers/mockFunctions.js
+++ src/tests/helpers/mockFunctions.js
@@ -6,7 +6,10 @@ export const mockFetch = (data) => {
 export const mockLocalStorage = (itemKey, itemValue) => {
   jest.spyOn(localStorage.__proto__, 'getItem');
   jest.spyOn(localStorage.__proto__, 'setItem');
+  // A linha seguinte serve para definir algum valor inicial no localStorage.
+  // É últil se o componente precisar desse valor na renderização inicial.
   localStorage.setItem(itemKey, itemValue);
+  
   localStorage.__proto__.setItem = jest.fn().mockImplementation((key, value) => localStorage[key] = value);
   localStorage.__proto__.getItem = jest.fn().mockImplementation((key) => localStorage[key])
 };
